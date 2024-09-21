981ccfadc3f9200c573efb7dbd9eee9256d946c3
Origamiccc <82864201+Origamiccc@users.noreply.github.com>
2024-09-22 01:21:14 +0800

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,46 @@
-// src/App.js
-import React, { useState } from "react";
-import Login from "./components/Login";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Forum from "./components/Forum";
 import Marketplace from "./components/Marketplace";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProfilePage from "./components/ProfilePage";
+import Login from "./components/Login";
+import axios from "axios";
+
 function App() {
-    const [account, setAccount] = useState("");
-    return (
-        <Router>
-            {account ? (
-                <Routes>
-                    <Route path="/" element={<Forum account={account} />} />
-                    <Route
-                        path="/marketplace"
-                        element={<Marketplace account={account} />}
-                    />
-                </Routes>
-            ) : (
-                <Login setAccount={setAccount} />
-            )}
-        </Router>
-    );
+  const [account, setAccount] = useState("");
+  const [userProfile, setUserProfile] = useState(null);
+
+  useEffect(() => {
+    if (account) {
+      axios
+        .get(`/api/users/${account}`)
+        .then((response) => setUserProfile(response.data))
+        .catch((error) => {
+          console.error("获取用户画像失败", error);
+          setUserProfile(null);
+        });
+    }
+  }, [account]);
+
+  if (!account) {
+    return <Login setAccount={setAccount} />;
+  }
+
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Forum account={account} userProfile={userProfile} />}
+        />
+        <Route
+          path="/marketplace"
+          element={<Marketplace account={account} userProfile={userProfile} />}
+        />
+        <Route path="/profile" element={<ProfilePage account={account} />} />
+      </Routes>
+    </Router>
+  );
 }
+
 export default App;
