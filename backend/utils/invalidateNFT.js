1515b69be6c0d7af94052d68768fbf8fe3d1f0fa
Nickel <80236011+Qi1e-Wu@users.noreply.github.com>
2024-09-21 18:19:54 +0000

    modify invalidateNFT

--- backend/utils/invalidateNFT.js
+++ backend/utils/invalidateNFT.js
@@ -8,12 +8,12 @@ const ContractAddress = Contract.address;
 const web3 = new Web3("http://127.0.0.1:8545"); // 替换为您的以太坊节点地址
 const contract = new web3.eth.Contract(ContractABI, ContractAddress);
 
-async function main() {
+async function invalidateNFT() {
     // 获取可用的账户列表
     const accounts = await web3.eth.getAccounts();
     const fromAccount = accounts[0]; // 使用第一个账户作为交易发送者
 
-    // **增加区块链时间**
+/*     // **增加区块链时间**
     // 增加一天的时间（86400 秒）
     await web3.currentProvider.send({
         jsonrpc: '2.0',
@@ -32,7 +32,7 @@ async function main() {
         id: new Date().getTime()
     }, (err, result) => {
         if (err) { console.error("挖掘新块出错：", err); }
-    }); 
+    });  */
 
     // **调用合约方法，将过期的 NFT 标记为无效**
     // 注意：需要使用 `.send` 方法发送交易，而不是 `.call`，因为需要修改区块链状态
@@ -45,6 +45,7 @@ async function main() {
     console.log("当前有效的 NFT下标（validCounter）: " + curIdx);
     console.log("当前所有的 NFT下标（tokenCounter）: " + (maxIdx - 1n));
     console.log("过期的 NFT 已被标记为无效");
+    return (maxIdx - 1n - curIdx);
 }
-
-//module.exports = { invalidateNFT };
+invalidateNFT();
+module.exports = { invalidateNFT };
